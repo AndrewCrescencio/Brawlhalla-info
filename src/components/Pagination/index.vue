4b4fc87ad4926746f659cd08a07a6afca0ff9ae9
AndrewCrescencio <andrew_workstation@hotmail.com>
2024-08-02 05:10:42 -0300

    feat: pagination component

---
+++ src/components/Pagination/index.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { useRoute } from 'nuxt/app'
+const route = useRoute()
+
+const { mode, region, page } = route.params
+</script>
+
+<template>
+  <div class="pagination">
+    <NuxtLink
+      v-if="Number(page) > 1"
+      role="button"
+      :to="{
+        name: 'rankings-mode-region-page',
+        params: { mode, region, page: Number(page) - 1 },
+      }"
+    >
+      <IconArrowBack />
+      <span class="sr-only">prev</span>
+    </NuxtLink>
+    <button v-else disabled>
+      <IconArrowBack />
+      <!-- prev -->
+      <span class="sr-only">prev</span>
+    </button>
+    <NuxtLink
+      role="button"
+      :to="{
+        name: 'rankings-mode-region-page',
+        params: { mode, region, page: Number(page) + 1 },
+      }"
+    >
+      <IconArrowFoward />
+      <span class="sr-only">next</span>
+    </NuxtLink>
+  </div>
+</template>
+<style lang="scss" scoped>
+.pagination {
+  display: flex;
+  justify-content: space-between;
+}
+.sr-only {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  white-space: nowrap;
+  border-width: 0;
+}
+</style>
