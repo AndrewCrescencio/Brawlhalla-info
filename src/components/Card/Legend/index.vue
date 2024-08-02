35a1d632987d02c1d789847c2d33ef1dbdffdfe2
AndrewCrescencio <andrew_workstation@hotmail.com>
2024-08-02 06:32:32 -0300

    fix: CardLegend

--- src/components/Card/Legend/index.vue
+++ src/components/Card/Legend/index.vue
@@ -7,31 +7,65 @@ defineProps({
 </script>
 
 <template>
-  <div class="card-legend">
-    <NuxtImg
-      class="card-legend__image"
-      :src="image"
-      :alt="`photo of the character ${name} from brawlhalla`"
-      loading="lazy"
-      width="134"
-      height="134"
-    />
+  <NuxtLink :to="{ name: 'legend-id', params: { id } }" class="card-legend">
+    <div class="card-legend__image">
+      <NuxtImg
+        :src="image"
+        :alt="`photo of the character ${name} from brawlhalla`"
+        loading="lazy"
+        width="120"
+        height="120"
+      />
+    </div>
+
     <p>{{ name }}</p>
-  </div>
+  </NuxtLink>
 </template>
 <style lang="scss" scoped>
 .card-legend {
   width: 100%;
   text-align: center;
+  text-decoration: none;
+  padding: 12px;
+
+  background-color: rgba(255, 255, 255, 0.05);
+  box-shadow: rgba(0, 0, 0, 0.2) 0px 2px 1px -1px,
+    rgba(0, 0, 0, 0.14) 0px 1px 1px 0px, rgba(0, 0, 0, 0.12) 0px 1px 3px 0px;
+
+  @media (prefers-color-scheme: light) {
+    background-color: #f3f3f3;
+
+    box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+      0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
+  }
+
   &__image {
-    width: 100%;
-    border-radius: 8px;
     margin-bottom: var(--pico-typography-spacing-vertical);
+
     @media (min-width: 768px) {
       margin-bottom: calc(var(--pico-typography-spacing-vertical) * 0.8);
     }
+
+    @media (prefers-color-scheme: light) {
+      background-color: #ddd;
+    }
+
+    img {
+      width: 100%;
+      border-radius: 8px;
+      transition: transform 0.2s;
+      position: relative;
+      margin: 0;
+
+      &:hover {
+        transform: scale(1.1);
+      }
+    }
   }
+
   p {
+    color: var(--pico-h1-color);
+    font-weight: 900;
     margin-bottom: 0;
   }
 }
