7c2006039b26e6860b77ddc0932579c9098ab2a2
AndrewCrescencio <andrew_workstation@hotmail.com>
2024-08-03 02:35:20 -0300

    fix(ui): Hero component

--- src/components/App/Hero/index.vue
+++ src/components/App/Hero/index.vue
@@ -1,10 +1,57 @@
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+const img = useImage()
+const backgroundStyles = computed(() => {
+  const imgUrl = img('/images/Banner_LegendGrid.webp')
+  return {
+    backgroundImage: `linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 0.9) 0%,
+      rgba(0, 0, 0, 0.9) 100%
+    ),url('${imgUrl}')`,
+  }
+})
+
+function useImage() {
+  throw new Error('Function not implemented.')
+}
+</script>
 <template>
-  <section class="appHero">
-    <h1>Brawlhalla Info</h1>
-    <h2>
-      information about <NuxtLink to="/legends">legends</NuxtLink> and
-      <NuxtLink to="/rankings/1v1/us-e/1">leaderboards</NuxtLink>
-    </h2>
+  <section class="appHero" :style="backgroundStyles">
+    <div class="container">
+      <h1>Brawlhalla Info</h1>
+      <p>
+        information about <NuxtLink to="/legends">legends</NuxtLink> and
+        <NuxtLink to="/rankings/1v1/us-e/1">leaderboards</NuxtLink>
+      </p>
+    </div>
   </section>
 </template>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.appHero {
+  // background-image: linear-gradient(
+  //     to bottom,
+  //     rgba(0, 0, 0, 0.9) 0%,
+  //     rgba(0, 0, 0, 0.9) 100%
+  //   ),
+  //   url('/images/Banner_LegendGrid.webp');
+  height: 70vh;
+  background-position: center center;
+  background-size: cover;
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100%;
+
+    h1,
+    p {
+      color: #fff;
+    }
+    p {
+      font-weight: 500;
+    }
+  }
+}
+</style>
