dd5170bdb94669ba7d10dc223b90edbca3df87a0
AndrewCrescencio <andrew_workstation@hotmail.com>
2024-08-08 17:54:48 -0300

    feat: SectionCtaLegends

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,11 @@
+<script lang="ts" setup>
+import { legendsMock } from '../mock/index.ts'
+
+const legends = ref([...legendsMock])
+</script>
+ref([...legendsMock])
+
 <template>
   <AppHero />
+  <SectionCtaLegends :legends="legends" />
 </template>
